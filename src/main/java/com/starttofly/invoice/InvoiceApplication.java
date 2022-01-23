7a6bdddadc73d5e6d3353fd6c29c7a8f56e30a1a
karima jamai <karimajamai.info@gmail.com>
2022-01-23 18:07:40 -0500

    BAR-1 : Test jenkins pipline.

--- src/main/java/com/starttofly/invoice/InvoiceApplication.java
+++ src/main/java/com/starttofly/invoice/InvoiceApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class InvoiceApplication {
 
 	public static void main(String[] args) {
+		System.out.print("**************************Hello word");
 		SpringApplication.run(InvoiceApplication.class, args);
 	}
 
